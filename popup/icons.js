fac62f3bb9dc9c43f04c4ead5281a8aa325eaa1e
Toloung <1172535606@qq.com>
2026-09-07 14:53:03 +0800

    style: compact player controls and clarify icons

--- popup/icons.js
+++ popup/icons.js
@@ -2,8 +2,8 @@ const paths = Object.freeze({
   note: '<path d="M14 3v11.2a3.5 3.5 0 1 1-2-3.15V6.4l8-1.7v8.8a3.5 3.5 0 1 1-2-3.15V3.2L14 4Z"/>',
   previous: '<path d="M6 5v14M18 6v12l-9-6 9-6Z"/>',
   next: '<path d="M18 5v14M6 6v12l9-6-9-6Z"/>',
-  play: '<path d="m9 6 10 6-10 6V6Z"/>',
-  pause: '<path d="M8 6h3v12H8V6Zm5 0h3v12h-3V6Z"/>',
+  play: '<path fill="currentColor" stroke="none" d="m9 5.5 10.5 6.5L9 18.5v-13Z"/>',
+  pause: '<path fill="currentColor" stroke="none" d="M7.5 5.5h3.6v13H7.5v-13Zm5.4 0h3.6v13h-3.6v-13Z"/>',
   back: '<path d="m14.5 5-7 7 7 7M8 12h9"/>'
 });
 
